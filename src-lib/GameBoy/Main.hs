0d2488f86ec31a60619d7c9520eccbaa39e57554
Johannes Maier <johannes.maier@active-group.de>
2023-09-03 22:25:11 +0200

    Show an SDL2 window

--- src-lib/GameBoy/Main.hs
+++ src-lib/GameBoy/Main.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module GameBoy.Main (main) where
 
+import Control.Concurrent.Async qualified as Async
 import Control.Monad
 import Control.Monad.State.Strict
 import Optics
+import SDL (($=))
+import SDL qualified
 import System.Environment qualified as Environment
 
 import GameBoy.BitStuff
@@ -35,6 +40,29 @@ main = do
     case args of
         [] -> fail "need path to ROM as first argument"
         (cartridgePath : _) -> do
-            bus <- initializeMemoryBus cartridgePath
-            _finalRegisters <- execStateT mainLoop (mkInitialState bus)
-            pure ()
+            graphics <- Async.async $ runGraphics
+            game <- Async.async $ do
+                bus <- initializeMemoryBus cartridgePath
+                _finalRegisters <- execStateT mainLoop (mkInitialState bus)
+                pure ()
+            void $ Async.waitAnyCancel [graphics, game]
+
+runGraphics :: IO ()
+runGraphics = do
+    SDL.initialize [SDL.InitVideo]
+    w <- SDL.createWindow "GameBoy emulator" SDL.defaultWindow
+    renderer <- SDL.createRenderer w (-1) SDL.defaultRenderer
+    SDL.rendererDrawColor renderer $= SDL.V4 0 0 255 255
+    void $ appLoop renderer
+  where
+    escPressed evt =
+        case SDL.eventPayload evt of
+            SDL.KeyboardEvent keyboardEvent ->
+                SDL.keyboardEventKeyMotion keyboardEvent == SDL.Pressed
+                    && SDL.keysymKeycode (SDL.keyboardEventKeysym keyboardEvent) == SDL.KeycodeEscape
+            _ -> False
+    appLoop renderer = do
+        evts <- SDL.pollEvents
+        SDL.clear renderer
+        SDL.present renderer
+        unless (any escPressed evts) (appLoop renderer)
