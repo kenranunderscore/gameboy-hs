f9d4d4251cfe1a7b025a0d6a2b2463adeca9a4bb
Johannes Maier <johannes.maier@active-group.de>
2023-10-13 21:34:56 +0200

    setup: improve the flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
-  description = "A very basic flake";
+  description =
+    "Development environment and build outputs for a GameBoy emulator";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
@@ -10,26 +11,40 @@
     };
   };
 
-  outputs = inputs @ {...}:
-    inputs.flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [(final: prev: {haskellPackages = prev.haskell.packages.ghc96;})];
-      pkgs = import inputs.nixpkgs {inherit system overlays;};
-    in {
-      formatter = pkgs.alejandra;
-      devShells.default = pkgs.haskellPackages.shellFor {
-        packages = _: [
-          (pkgs.haskellPackages.callCabal2nix "game-boy-emulator-hs"
-            (pkgs.lib.cleanSource ./.) {})
+  outputs = inputs@{ self, ... }:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+          (final: prev: { haskellPackages = prev.haskell.packages.ghc96; })
+          self.overlays.default
         ];
-        nativeBuildInputs = [
-          pkgs.alejandra
-          pkgs.cabal-install
-          pkgs.haskellPackages.haskell-language-server
-          pkgs.haskellPackages.cabal-fmt
-          (pkgs.haskell.lib.compose.dontCheck
-            (pkgs.haskellPackages.callCabal2nix "fourmolu" inputs.fourmolu
-              {}))
-        ];
+        pkgs = import inputs.nixpkgs { inherit system overlays; };
+      in {
+        formatter = pkgs.alejandra;
+
+        devShells.default = pkgs.haskellPackages.shellFor {
+          packages = p: [ p.game-boy-emulator-hs ];
+          nativeBuildInputs = [
+            pkgs.alejandra
+            pkgs.cabal-install
+            pkgs.haskellPackages.haskell-language-server
+            pkgs.haskellPackages.cabal-fmt
+            (pkgs.haskell.lib.compose.dontCheck
+              (pkgs.haskellPackages.callCabal2nix "fourmolu" inputs.fourmolu
+                { }))
+          ];
+        };
+      }) // {
+        overlays.default = final: prev: {
+          haskellPackages = prev.haskellPackages.override (old: {
+            overrides =
+              final.lib.composeExtensions (old.overrides or (_: _: { }))
+              (hfinal: hprev: {
+                game-boy-emulator-hs =
+                  hfinal.callCabal2nix "game-boy-emulator-hs"
+                  (final.lib.cleanSource ./.) { };
+              });
+          });
+        };
       };
-    });
 }
